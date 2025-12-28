5b17e675dc8fc8c096389319c121d915e9c93b73
dandredurkofficial-art <dandredurkofficial@gmail.com>
2025-12-28 00:16:58 -0800

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,27 @@
+// LOGIN
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "index.html";
+    })
+    .catch(error => {
+      alert(error.message);
+    });
+}
+
+// REGISTER
+function register() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "index.html";
+    })
+    .catch(error => {
+      alert(error.message);
+    });
+}
