ed2dcdc426e5bba9f7c0e82bc179524dfa95fcd5
dandredurkofficial-art <dandredurkofficial@gmail.com>
2026-01-01 03:33:21 +0300

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,1 +1,10 @@
+// checkout.js
+const beat = JSON.parse(localStorage.getItem("checkoutBeat"));
 
+if (!beat) {
+  document.body.innerHTML = "No beat selected";
+}
+
+window.payNow = () => {
+  window.location.href = `/functions/createPayPalPayment?price=${beat.price}`;
+};
