7c15364692dd11d1b38605cdf54784da6c4e4acc
dandredurkofficial-art <dandredurkofficial@gmail.com>
2026-01-03 06:29:54 -0800

    Update auth-ui.js

--- auth-ui.js
+++ auth-ui.js
@@ -1,53 +1,26 @@
-<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
   getAuth,
-  onAuthStateChanged,
   signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signOut
+  createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAlh6_jXAJ2Wdyfw04Ieb9NqIoa8ZziuxE",
   authDomain: "prodbybigi.firebaseapp.com",
-  projectId: "prodbybigi",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-/* 🔐 AUTH GUARD */
-onAuthStateChanged(auth, user => {
-  const page = location.pathname;
-
-  if (user && (page.includes("login") || page.includes("register"))) {
-    location.replace("dashboard.html");
-  }
-
-  if (!user && page.includes("dashboard")) {
-    location.replace("login.html");
-  }
-});
-
-/* LOGIN */
 window.loginUser = () => {
-  signInWithEmailAndPassword(
-    auth,
-    email.value,
-    password.value
-  ).catch(err => alert(err.message));
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then(() => location.href = "dashboard.html")
+    .catch(err => alert(err.message));
 };
 
-/* REGISTER */
 window.registerUser = () => {
-  createUserWithEmailAndPassword(
-    auth,
-    email.value,
-    password.value
-  ).catch(err => alert(err.message));
+  createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then(() => location.href = "dashboard.html")
+    .catch(err => alert(err.message));
 };
-
-/* LOGOUT */
-window.logoutUser = () => signOut(auth);
-</script>
