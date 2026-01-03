132a987082f3b7adfc9be0833cc6d0047a1c5033
dandredurkofficial-art <dandredurkofficial@gmail.com>
2026-01-03 14:35:12 -0800

    Create beats-data.js

---
+++ js/beats-data.js
@@ -0,0 +1,27 @@
+// THIS WILL LATER COME FROM FIREBASE
+const beats = [
+  {
+    id: 1,
+    title: "Midnight Drip",
+    producer: "bigi",
+    producerName: "Prod. Bigi",
+    price: 39,
+    genre: "Trap"
+  },
+  {
+    id: 2,
+    title: "Cold Streets",
+    producer: "bigi",
+    producerName: "Prod. Bigi",
+    price: 29,
+    genre: "Drill"
+  },
+  {
+    id: 3,
+    title: "Skyline",
+    producer: "alex",
+    producerName: "Prod. Alex",
+    price: 49,
+    genre: "Hip Hop"
+  }
+];
