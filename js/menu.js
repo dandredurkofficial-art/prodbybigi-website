3abdcd3406a714facbc02b9513592c26133ab9a2
dandredurkofficial-art <dandredurkofficial@gmail.com>
2026-01-09 13:11:04 -0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,14 @@
+const menuBtn =
+  document.getElementById("menuBtn") ||
+  document.getElementById("hamburger");
 
+const mobileMenu =
+  document.getElementById("mobileMenu") ||
+  document.querySelector(".menu");
+
+if (menuBtn && mobileMenu) {
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("open");
+  });
+}
