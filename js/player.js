4cd805da09f5d0faf27d71a83ee1ea35e9e79d2d
dandredurkofficial-art <dandredurkofficial@gmail.com>
2026-01-16 15:55:26 -0800

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,17 +1,14 @@
-// /js/player.js
 let currentAudio = null;
 
 document.addEventListener("click", e => {
   const btn = e.target.closest("[data-audio]");
   if (!btn) return;
 
-  const src = btn.dataset.audio;
-
   if (currentAudio) {
     currentAudio.pause();
     currentAudio = null;
   }
 
-  currentAudio = new Audio(src);
+  currentAudio = new Audio(btn.dataset.audio);
   currentAudio.play();
 });
