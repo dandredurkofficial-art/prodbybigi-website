c065f0a5986eea7571e182fd74510af41b407f5e
dandredurkofficial-art <dandredurkofficial@gmail.com>
2026-01-16 15:34:36 -0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
+// /js/menu.js
 document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("menuBtn");
-  const menu = document.getElementById("mobileMenu");
+  const btn = document.querySelector(".menu-btn");
+  const menu = document.querySelector(".mobile-menu");
 
   if (!btn || !menu) return;
 
