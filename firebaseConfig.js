23079f13aaa4588a4f9b994a6a0236040e1c33dc
hanselbeatz <officialbigi254@gmail.com>
2026-02-09 06:21:40 -0800

    Add Firebase web config and fix imports

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,4 @@
-
-firefireff
-cimport { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
