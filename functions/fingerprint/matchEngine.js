b656daf91ca9f7c89ff39485b23a7e52e956e7fe
dandredurkofficial-art <dandredurkofficial@gmail.com>
2026-05-10 20:44:03 +0300

    Add compareFingerprints function for matching

--- functions/fingerprint/matchEngine.js
+++ functions/fingerprint/matchEngine.js
@@ -1,1 +1,16 @@
+function compareFingerprints(a, b) {
+  const setA = new Set(a.landmarks.map(l => l.hash));
+  const setB = new Set(b.landmarks.map(l => l.hash));
 
+  let match = 0;
+
+  setA.forEach(h => {
+    if (setB.has(h)) match++;
+  });
+
+  return match / Math.max(setA.size, setB.size);
+}
+
+module.exports = {
+  compareFingerprints
+};
